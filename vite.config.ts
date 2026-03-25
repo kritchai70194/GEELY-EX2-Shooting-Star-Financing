9083b53fc7d5a83be01a70b6630ab09137bcdf9c
ARKN <63953889+kritchai70194@users.noreply.github.com>
2026-03-26 00:31:07 +0700

    Create vite.config.ts file

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/GEELY-EX2-Shooting-Star-Financing/',
+})
